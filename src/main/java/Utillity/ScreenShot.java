1afdf7fea205091a328160a2cf1fab4a4ad3e7c4
kedar-16 <kedarpatilkp16@gmail.com>
2022-08-12 19:16:27 +0530

    new update

--- src/main/java/Utillity/ScreenShot.java
+++ src/main/java/Utillity/ScreenShot.java
@@ -2,6 +2,8 @@ package Utillity;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -12,6 +14,7 @@ public class ScreenShot {
 	
 	public static void TakeScreenShot(WebDriver driver,String filename)
 	{
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
 		TakesScreenshot ss=(TakesScreenshot) driver;
 		File source = ss.getScreenshotAs(OutputType.FILE);
 		File destination=new File(".\\Screenshot\\ "+filename+".jpeg");
